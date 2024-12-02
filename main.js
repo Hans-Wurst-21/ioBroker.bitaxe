f17038f660c56cfae0a97c99b726fc9f7a0a2134
Hans-Wurst-21 <github@hansmail.net>
2024-12-02 13:11:22 +0100

    use axios, basis for main.js and io-package.json create

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+'use strict';
+
 const utils = require('@iobroker/adapter-core');
-const WebSocket = require('ws');
+const axios = require('axios');
 
 class Bitaxe extends utils.Adapter {
     constructor(options) {
@@ -12,53 +14,103 @@ class Bitaxe extends utils.Adapter {
     }
 
     async onReady() {
-        this.log.info('Bitaxe is ready');
+        this.log.info('Bitaxe adapter is starting');
 
-        // Websocket-URL für Testzwecke
-        const websocketUrl = 'wss://echo.websocket.org';
+        this.apiUrl = this.config.apiUrl || 'http://192.168.178.26/api/system/info';
+        this.updateInterval = this.config.updateInterval || 60000;
 
-        this.connectWebsocket(websocketUrl);
+        this.fetchDataInterval = setInterval(() => {
+            this.fetchApiData();
+        }, this.updateInterval);
+
+        this.fetchApiData();
     }
 
-    connectWebsocket(url) {
-        this.ws = new WebSocket(url);
+    async fetchApiData() {
+        try {
+            if (typeof this.apiUrl !== 'string') {
+                throw new Error('API URL ist nicht definiert');
+            }
+            const response = await axios.get(this.apiUrl);
+            this.log.debug(`Raw data from API: ${JSON.stringify(response.data)}`);
+            await this.setStateAsync('info.connection', { val: true, ack: true });
+            await this.processApiData(response.data);
+        } catch (error) {
+            this.log.error(`Error fetching data: ${error.message}`);
+            await this.setStateAsync('info.connection', { val: false, ack: true });
+        }
+    }
 
-        this.ws.on('open', () => {
-            this.log.info('Websocket connection established');
-            // Hier können Sie eine Testnachricht senden, wenn nötig
-            // this.ws.send('Hello Server');
-        });
+    async processApiData(data) {
+        for (const [key, value] of Object.entries(data)) {
+            const stateId = this.getStateId(key);
+            if (stateId) {
+                await this.setStateAsync(stateId, { val: value, ack: true });
+            }
+        }
+    }
 
-        this.ws.on('message', data => {
-            console.log('Received from websocket:', data.toString());
-        });
-
-        this.ws.on('error', error => {
-            this.log.error(`Websocket error: ${error}`);
-        });
-
-        this.ws.on('close', () => {
-            this.log.info('Websocket connection closed');
-        });
+    getStateId(key) {
+        const mapping = {
+            power: 'power.power',
+            voltage: 'power.voltage',
+            current: 'power.current',
+            temp: 'temperature.temp',
+            vrTemp: 'temperature.vrTemp',
+            hashRate: 'mining.hashRate',
+            bestDiff: 'mining.bestDiff',
+            bestSessionDiff: 'mining.bestSessionDiff',
+            isUsingFallbackStratum: 'mining.isUsingFallbackStratum',
+            sharesAccepted: 'mining.sharesAccepted',
+            sharesRejected: 'mining.sharesRejected',
+            freeHeap: 'hardware.freeHeap',
+            coreVoltage: 'hardware.coreVoltage',
+            coreVoltageActual: 'hardware.coreVoltageActual',
+            frequency: 'hardware.frequency',
+            asicCount: 'hardware.asicCount',
+            smallCoreCount: 'hardware.smallCoreCount',
+            ASICModel: 'hardware.ASICModel',
+            ssid: 'network.ssid',
+            macAddr: 'network.macAddr',
+            hostname: 'network.hostname',
+            wifiStatus: 'network.wifiStatus',
+            stratumURL: 'stratum.stratumURL',
+            fallbackStratumURL: 'stratum.fallbackStratumURL',
+            stratumPort: 'stratum.stratumPort',
+            fallbackStratumPort: 'stratum.fallbackStratumPort',
+            stratumUser: 'stratum.stratumUser',
+            fallbackStratumUser: 'stratum.fallbackStratumUser',
+            version: 'system.version',
+            boardVersion: 'system.boardVersion',
+            runningPartition: 'system.runningPartition',
+            uptimeSeconds: 'system.uptimeSeconds',
+            flipscreen: 'configuration.flipscreen',
+            overheat_mode: 'configuration.overheat_mode',
+            invertscreen: 'configuration.invertscreen',
+            invertfanpolarity: 'configuration.invertfanpolarity',
+            autofanspeed: 'configuration.autofanspeed',
+            fanspeed: 'configuration.fanspeed',
+            fanrpm: 'configuration.fanrpm',
+        };
+        return mapping[key] || null;
     }
 
     onUnload(callback) {
         try {
-            if (this.ws) {
-                this.ws.close();
+            if (this.fetchDataInterval) {
+                clearInterval(this.fetchDataInterval);
             }
+            this.log.info('Cleaned up everything...');
             callback();
         } catch (e) {
-            this.log.error(`Error during unload: ${e}`);
+            this.log.error(`Fehler beim Beenden des Adapters: ${e.message}`);
             callback();
         }
     }
 }
 
 if (require.main !== module) {
-    // Export the constructor in compact mode
     module.exports = options => new Bitaxe(options);
 } else {
-    // otherwise start the instance directly
     new Bitaxe();
 }
